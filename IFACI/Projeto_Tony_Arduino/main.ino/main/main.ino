c8b0ffde8859acad533da280a0f35b9999ebbd74
Pedro henrique rodrigues leme <151878192+PhrL0@users.noreply.github.com>
2025-04-29 19:01:05 -0300

    Update main.ino

--- IFACI/Projeto_Tony_Arduino/main.ino/main/main.ino
+++ IFACI/Projeto_Tony_Arduino/main.ino/main/main.ino
@@ -1,5 +1,9 @@
-#include "sensor_temp.h"
-#include "sirene.h"
+
+#include "DHT.h"
+ 
+#define DHTPIN A1 // pino que estamos conectado
+#define DHTTYPE DHT11 // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
 
 const int LED_GREEN_PIN = 5;
 const int LED_RED_PIN = 6;
@@ -12,11 +16,10 @@ int potValue = 0;
 
 void setup() {
   Serial.begin(9600);
-  startTemperature();
+  dht.begin();
   pinMode(LED_GREEN_PIN, OUTPUT);
   pinMode(LED_RED_PIN, OUTPUT);
   pinMode(POTENTIOMETER_PIN, INPUT);
-  pinMode(12, OUTPUT);
 }
 
 void loop() {
@@ -26,25 +29,16 @@ void loop() {
   if(buttonState == 1){
     sendSensorData();
     digitalWrite(LED_GREEN_PIN, HIGH);
-    digitalWrite(LED_RED_PIN, LOW);
   } 
   if(buttonState == 0) {
     digitalWrite(LED_GREEN_PIN, LOW);
-    digitalWrite(LED_RED_PIN, HIGH);
   }
   delay(1500);
 }
 
 void sendSensorData() {
   potValue = analogRead(POTENTIOMETER_PIN);
-  temp = readTemperature();
-
-  if(potValue >= 500){
-    startSirene();
-  } 
-  if(potValue == 200) {
-    stopSirene();
-  }
+  temp = dht.readTemperature();
   limparSerial();
   Serial.println(String(potValue) + "," + String(temp));
   
